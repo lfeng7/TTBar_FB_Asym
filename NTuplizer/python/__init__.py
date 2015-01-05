894b75deca9df25b8ca7ef6c3b84c88022682d9b
Nicholas <nick.eminizer@gmail.com>
2015-01-05 13:37:10 -0600

    Incorporated first pass full framework B2G nTuplizer with prunedGenParticles saved

--- NTuplizer/python/__init__.py
+++ NTuplizer/python/__init__.py
@@ -1,3 +1,3 @@
 #Automatically created by SCRAM
 import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Analysis/MiniAnalyzer/',1)[0])+'/cfipython/slc6_amd64_gcc481/Analysis/MiniAnalyzer')
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/B2GAnaFW/B2GAnaFW/',1)[0])+'/cfipython/slc6_amd64_gcc481/B2GAnaFW/B2GAnaFW')
